a14dbb08d5adf4ff2f8c11899e75c5cfaaac50b0
hitesh-ag1 <hitesh001@e.ntu.edu.sg>
2023-02-22 13:52:34 +0530

    updated form for selecting data sites

--- src/components/SiteForm.jsx
+++ src/components/SiteForm.jsx
@@ -24,21 +24,17 @@ export default function SiteForm() {
         },
     });
     return (
-        <div>
-        <p className='text-2xl'>Step 1: Select Data Sites</p>
+        <div className='w-full min-w-[500px]'>
+        <p className='pb-8 text-left text-2xl'>Step 1: Select Data Sites</p>
     <form >
     <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3">
       <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-        Full Name
+        Data Sites
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe" />
-    </div>
-  </div>
-
-          <Listbox multiple value={selected} onChange={setSelected} className="w-[60%] max-w-full min-w-[200px]">
+    <Listbox multiple value={selected} onChange={setSelected} className="w-full max-w-xs min-w-[150px]">
         <div className="relative mt-1">
           <Listbox.Button className="border-2 border-gray-200 rounded relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
             <span className="block truncate">{selected.map(item => item.value).join(', ')}</span>
@@ -88,44 +84,14 @@ export default function SiteForm() {
           </Transition>
         </div>
       </Listbox>
+    </div>
+  </div>
+  <div class="grid justify-items-center pt-4">
+      <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+        Submit
+      </button>
+  </div>
 
-    {/* <div class="md:flex md:items-center mb-6">
-      <div class="md:w-1/3">
-        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-          Full Name
-        </label>
-      </div>
-      <div class="md:w-2/3">
-        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe" />
-      </div>
-    </div>
-    <div class="md:flex md:items-center mb-6">
-      <div class="md:w-1/3">
-        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
-          Password
-        </label>
-      </div>
-      <div class="md:w-2/3">
-        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************" />
-      </div>
-    </div>
-    <div class="md:flex md:items-center mb-6">
-      <div class="md:w-1/3"></div>
-      <label class="md:w-2/3 block text-gray-500 font-bold">
-        <input class="mr-2 leading-tight" type="checkbox" />
-        <span class="text-sm">
-          Send me your newsletter!
-        </span>
-      </label>
-    </div>
-    <div class="md:flex md:items-center">
-      <div class="md:w-1/3"></div>
-      <div class="md:w-2/3">
-        <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-          Sign Up
-        </button>
-      </div>
-    </div> */}
   </form>
   </div>
 );
